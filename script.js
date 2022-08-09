9eb2c403ee0a68dc28e4ba10a5ac52b2fa15ec60
Petrut Dragos Ivanoiu <petrut.ivanoiu@visma.com>
2022-08-09 14:48:04 +0300

    feat: add functionality to make an array from text and generate table

--- script.js
+++ script.js
@@ -1,15 +1,47 @@
-const TextArea1 = document.getElementById("text1");
-const TextArea2 = document.getElementById("text2");
-const CheckDiffBtn = document.getElementById("check-diff-btn");
+const textArea1 = document.getElementById("text1");
+const textArea2 = document.getElementById("text2");
+const checkDiffBtn = document.getElementById("check-diff-btn");
 
 // get the text from the textarea and put it in the second textarea
-
+let text = `hellow                 world how are you
+today is a good day`;
 // add event listener to check diff button
-CheckDiffBtn.addEventListener("click", function () {
-  console.log(checkEndOfLine(TextArea1.value));
+checkDiffBtn.addEventListener("click", function () {
+  console.log(textToArray(text, "\n"));
+  let newText = textToArray(textArea1.value, "\n");
+  console.log(newText);
+  newText[0] = removeWhiteSpace(newText[0]);
+  console.log(newText);
 });
 
-// create a function to check end of a line
-function checkEndOfLine(text) {
-  text.charAt(2);
+function textToArray(text, separator = " ") {
+  let textArray = text.split(separator);
+  return textArray;
+}
+
+// create a function that removes the white space from the text
+function removeWhiteSpace(text) {
+  let newText = text.replace(/\s+/g, " ");
+  return newText;
+}
+
+// create a function that generates a table
+function generateTable(data) {
+  const tbl = document.createElement("table");
+  const tblBody = document.createElement("tbody");
+
+  for (let i = 0; i < data.length; i++) {
+    const row = document.createElement("tr");
+    for (let j = 0; j < data[i].length; j++) {
+      const cell = document.createElement("td");
+      const cellText = document.createTextNode(data[i][j]);
+      cell.appendChild(cellText);
+      row.appendChild(cell);
+    }
+    tblBody.appendChild(row);
+  }
+
+  tbl.appendChild(tblBody);
+  document.body.appendChild(tbl);
+  tbl.setAttribute("border", "2");
 }
